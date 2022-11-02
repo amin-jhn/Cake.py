9cb5cde1107bf05a16f117f0dc40fc3bfd24eda3
amin-jhn <96478547+amin-jhn@users.noreply.github.com>
2022-11-02 12:31:51 +0330

    Initial commit

---
+++ Cake.py
@@ -0,0 +1,6 @@
+li = list(map(int,input().split()))
+n = li[0]
+k = li[1]
+temp = list(map(int,input().split()))
+cakes = temp
+Goodies = []
---
+++ info.py
@@ -0,0 +1,18 @@
+FName = "My first name is Ameneh "
+LName = "My last name is Taji"
+age = 21
+National_Code = "0926597280"
+Gender = "Female"
+Phone_Number = "09390545139"
+Marital_Status = "Single"
+Etic = "Iranian"
+Birthday_Date = "17 May"
+print(FName)
+print(LName)
+print(age)
+print(National_Code)
+print(Gender)
+print(Phone_Number)
+print(Marital_Status)
+print(Etic)
+print(Birthday_Date)
